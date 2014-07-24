a25654ed7a17edfeba305c89df03bc7e1f327915
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-07-24 23:44:30 +0300

    Follow picoweb request/response args refactor.

--- views.py
+++ views.py
@@ -13,7 +13,7 @@ def get_page():
     return min(int(page), 1)
 
 @app.route('/', methods=['GET', 'POST'])
-def homepage(writer, request):
+def homepage(request, response):
     if request.method == 'POST':
         print(request.headers)
         yield from request.read_form_data()
@@ -21,21 +21,21 @@ def homepage(writer, request):
             note_id = Note.create(content=request.form['content'][0])
             note = list(Note.get_id(note_id))[0]
             rendered = picoweb.render_str('note', (note,))
-            yield from picoweb.jsonify(writer, {'note': rendered, 'success': 1})
+            yield from picoweb.jsonify(response, {'note': rendered, 'success': 1})
             return
 
-        yield from picoweb.jsonify(writer, {'success': 0})
+        yield from picoweb.jsonify(response, {'success': 0})
         return
 
-    yield from picoweb.start_response(writer)
+    yield from picoweb.start_response(response)
 #    notes = Note.public().paginate(get_page(), 50)
     notes = list(Note.public())
-    yield from picoweb.render(writer, 'homepage', (notes,))
+    yield from picoweb.render(response, 'homepage', (notes,))
 
 @app.route(re.compile('^/archive/(\d+)'), methods=['POST'])
-def archive_note(writer, request):
+def archive_note(request, response):
     print("archive_note", request.url_match.group(1))
     Note.update({"id": request.url_match.group(1)}, archived=1)
-    yield from picoweb.jsonify(writer, {'success': True})
+    yield from picoweb.jsonify(response, {'success': True})
 
-app.add_url_rule('/js/notes.js', lambda w, req: (yield from picoweb.sendfile(w, "js/notes.js")))
+app.add_url_rule('/js/notes.js', lambda req, resp: (yield from picoweb.sendfile(resp, "js/notes.js")))
