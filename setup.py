d41a8b69fc6d527fb06beb2612a86eb10347e1e2
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-05-15 01:01:54 +0300

    Release 0.7.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import optimize_upip
 
 
 setup(name='notes-pico',
-      version='0.6.3',
+      version='0.7',
       description="""A note-taking example web application for Picoweb
 web pico-framework. (Ported from Flask original)""",
       long_description=open('README.rst').read(),
