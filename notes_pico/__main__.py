a8740d3c5dafc7602da6d56fba2222bf74a31691
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-06-07 02:39:31 +0300

    __main__: Allow to pass arbitrary params to picoweb's WebApp.run().

--- notes_pico/__main__.py
+++ notes_pico/__main__.py
@@ -1,12 +1,12 @@
 from .app import app
 from . import views
 
-def main():
+def main(**params):
     import logging
     logging.basicConfig(level=logging.INFO)
     import micropython
     micropython.mem_info()
-    app.run(debug=True)
+    app.run(debug=True, **params)
 
 if __name__ == '__main__':
     main()
