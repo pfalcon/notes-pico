68be076ecd82b8640e267d6c95aad46dc5c47d1e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-06-07 01:53:08 +0300

    __main__: Add main() function to allow to run app via import.

--- notes_pico/__main__.py
+++ notes_pico/__main__.py
@@ -1,9 +1,12 @@
 from .app import app
 from . import views
 
-if __name__ == '__main__':
+def main():
     import logging
     logging.basicConfig(level=logging.INFO)
     import micropython
     micropython.mem_info()
     app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
