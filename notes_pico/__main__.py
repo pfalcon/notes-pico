ea3b67ab6392285aeb245adabb52f1f9822bbc13
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-06-10 17:46:17 +0300

    __main__: Preload templates and gc.collect() to avoid mem fragmentation.
    And to allow to run on low-heap systems like esp8266.

--- notes_pico/__main__.py
+++ notes_pico/__main__.py
@@ -1,9 +1,18 @@
 from .app import app
 from . import views
+import gc
 
 def main(**params):
+    gc.collect()
     import logging
     logging.basicConfig(level=logging.INFO)
+
+    # Preload templates to avoid memory fragmentation issues
+    gc.collect()
+    app._load_template('homepage.html')
+    app._load_template('note.html')
+    gc.collect()
+
     import micropython
     micropython.mem_info()
     app.run(debug=True, **params)
